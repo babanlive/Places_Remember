f8dab2aabbbd43524f7553a7e9650be61d13be3e
Mikhail Artemov <babanlive@gmail.com>
2024-05-28 21:58:58 +0700

    commit test

---
+++ places/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Place
+
+
+class PlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ['title', 'comment', 'locations']
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from allauth.account.views import logout
+from django.shortcuts import redirect
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('places:index')
